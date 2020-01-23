9bc4ea8206ba649d78298478d04dafb204796436
ogundimu1 <abolajic@gmail.com>
2020-01-23 21:45:10 +0100

    Updates

---
+++ precache-manifest.4376e3792e1a18db210f21c3f3335607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a380c6c2ae86399fc3b22fe9138336",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "0645dee3bea4d2dd3405",
+    "url": "/Todo-List/static/css/main.0fbefffc.chunk.css"
+  },
+  {
+    "revision": "8bfbf08d3a77abc3955c",
+    "url": "/Todo-List/static/js/2.5a1e2f6e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Todo-List/static/js/2.5a1e2f6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0645dee3bea4d2dd3405",
+    "url": "/Todo-List/static/js/main.ce1c3c18.chunk.js"
+  },
+  {
+    "revision": "3fca894692e6ba9a968b",
+    "url": "/Todo-List/static/js/runtime-main.c5a51d1b.js"
+  }
+]);
